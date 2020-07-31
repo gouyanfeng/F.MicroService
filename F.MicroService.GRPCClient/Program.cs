9f8d11ea25443ffafa3cc590eeacd408a5760774
gyf <yanfeng.gou@zhan.com>
2020-07-31 14:40:35 +0800

    添加项目文件。

---
+++ F.MicroService.GRPCClient/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using Grpc.Net.Client;
+using System;
+
+namespace F.MicroService.GRPCClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+
+            var client = new Greeter.GreeterClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "yf" });
+            Console.WriteLine(reply.Message);
+            Console.ReadKey();
+        }
+    }
+
+}
